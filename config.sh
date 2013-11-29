13c72e9700e0656eeced572686ebda2eda1d796d
root <root@CentOS-zf.(none)>
2013-11-29 20:35:46 +0800

    test

---
+++ config.sh
@@ -0,0 +1,9 @@
+mv ~/.vimrc ~/.vimrc_bk
+mv ~/.vim ~/.vim_bk
+mv ~/.bashrc ~/.bashrc_bk
+
+ln -s ~/git_rep/work_out/.vimrc ~/.vimrc
+ln -s ~/git_rep/work_out/.vim ~/.vim
+ln -s ~/git_rep/work_out/.bashrc ~/.bashrc
+ln -s ~/git_rep/work_out/.tmux.conf ~/.tmux.conf
+
