a11f2376c47c54f8b3b17bef68b73922356cd048
赵峰 <zhaofeng@eduzhixin.com>
2019-05-09 10:40:01 +0800

    add file.vim

--- config.sh
+++ config.sh
@@ -15,3 +15,5 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+cp file.vim  ~/.vim/bundle/FuzzyFinder/autoload/fuf/file.vim
